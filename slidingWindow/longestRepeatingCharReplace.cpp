9264561c50d16fbea6524d3e2909edf725d575dd
shashwat <shashwatw.1906@gmail.com>
2025-07-03 14:47:10 +0530

    Implement optimal solution for longest repeating character replacement problem

--- slidingWindow/longestRepeatingCharReplace.cpp
+++ slidingWindow/longestRepeatingCharReplace.cpp
@@ -53,5 +53,26 @@ int characterReplacement(string s, int k) {
      }  
     return maxl;
 }
+int optimal(string s, int k) {
+    int r = 0;
+    int l = 0;
+    int maxf = 0;
+    int maxl =0;
+    vector<int>hash(26 , 0);
+    while(r < s.size()){
+        hash[s[r] - 'A']++;
+        maxf = max(maxf , hash[s[r] - 'A']);
+        if(r-l+1 - maxf > k){
+            hash[s[l] -'A']--;
+            l++;
+        }
+        if(r-l+1 - maxf <= k){
+            maxl = max(maxl , r-l+1);
+        }
+        r++;
+     }  
+    return maxl;
+}
 int main(){
+    cout<<optimal("AABABBA" , 1);
 }
